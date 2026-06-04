168a101eb1e82d7e996573da64159fb1178ec263
srivasdev <devanshsri14@hotmail.com>
2026-06-04 21:02:19 +0530

    Variables

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Namaste Dev" << endl;
+cout  << "Namaste Duniya" << endl;
+cout << "Hello only";
+    return 0;
+
+}
---
+++ variable.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+using namespace std;
+int main(){
+
+    //variable define
+// int age = 50;
+// cout << "Dev's age is:" << age << endl;
+
+// declaration
+// int age;
+
+
+// definition
+// int age =20;
+// cout << age << endl;
+// //  test commit
+
+// // manipulation or updation
+// age = 101;
+// cout << age << endl;
+// int
+// int count = 5;
+// // float
+// float share = 1.54;
+// // char
+// char alphabet = 'd';
+// // double
+// double weight = 55.69875;
+// // boolean
+// bool isMale = true;
+
+
+
+// cout << count << endl;
+// cout << share << endl;
+// cout << alphabet << endl;
+// cout << weight << endl;
+// cout << isMale << endl;
+int age = 20;
+cout << sizeof(age) << endl;
+return 0;
+}
