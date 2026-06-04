d2908932b2d57471477f6026ae678e0fed3bbc1f
srivasdev <devanshsri14@hotmail.com>
2026-06-04 21:32:04 +0530

    io study

---
+++ Userinput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+int age;
+cout << "Enter your age:" << endl;
+cin >> age;
+cout << "Yes!Your age is:" << age << endl;
+
+{
+    int marks;
+    cout << "Enter your marks:" << endl;
+    cin >> marks;
+    cout << marks <<" is very low marks." << endl;
+}
+
+ return 0;
+}
