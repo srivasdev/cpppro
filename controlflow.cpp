21d9c61d7cf266aaeee6f06d27a4142e91f4050b
srivasdev <devanshsri14@hotmail.com>
2026-06-05 21:44:04 +0530

    Control Flow

---
+++ controlflow.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int budget;
+cout << "Enter your Budget:" << endl;
+
+// input
+cin >> budget;
+
+if(budget > 2000000){
+    cout << "You can buy Scorpio" << endl;
+
+}
+
+return 0;
+
+}
