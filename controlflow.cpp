d9cca7012eb9290123ecc9ee5d43cf8c4f3f7271
srivasdev <devanshsri14@hotmail.com>
2026-06-05 22:17:45 +0530

    if-else-if

--- controlflow.cpp
+++ controlflow.cpp
@@ -3,20 +3,40 @@ using namespace std;
 
 int main(){
 
-int budget;
-cout << "Enter your Budget:" << endl;
+// int budget;
+// cout << "Enter your Budget:" << endl;
 
-// input
-cin >> budget;
+// // input
+// cin >> budget;
 
-if(budget > 2000000){
-    cout << "You can buy Scorpio" << endl;
+// if(budget > 2000000){
+//     cout << "You can buy Scorpio" << endl;
 
-}
+// }
   
-else{
-cout << "You can't bro" << endl;
+// else{
+// cout << "You can't bro" << endl;
+// }
+
+int marks;
+cout << "Enter your marks:" << endl;{
+    cin >> marks;
 }
+
+if(marks > 90){
+    cout << "A" << endl;
+}
+else if (marks >80){
+cout << "B" << endl; 
+}
+else if (marks >70){
+    cout << "C" << endl;
+}  
+else if (marks >60){
+    cout << "D" << endl;
+}
+
+
 return 0;
 
 }
