16922b12d1ef882cca53862ab1e059d78ff53bdd
srivasdev <devanshsri14@hotmail.com>
2026-06-06 22:43:20 +0530

    Ternary

---
+++ ternary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+//     int age =22;
+
+// (age > 18) ? cout << "Can vote" : cout << "Cannot Vote" ;
+
+int x = 10;
+int y = 20;
+
+int result = (x > y) ? x:y;
+
+cout << "result:" << result << endl;
+
+
+return 0;
+}
