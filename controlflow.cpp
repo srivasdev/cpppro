2b3c5db0c075fbf47815cf33d58f643be6d2e078
srivasdev <devanshsri14@hotmail.com>
2026-06-06 21:29:35 +0530

    FlowComp

--- controlflow.cpp
+++ controlflow.cpp
@@ -18,23 +18,50 @@ int main(){
 // cout << "You can't bro" << endl;
 // }
 
-int marks;
-cout << "Enter your marks:" << endl;{
-    cin >> marks;
+// int marks;
+// cout << "Enter your marks:" << endl;{
+//     cin >> marks;
+// }
+
+// if(marks > 90){
+//     cout << "A" << endl;
+// }
+// else if (marks >80){
+// cout << "B" << endl; 
+// }
+// else if (marks >70){
+//     cout << "C" << endl;
+// }  
+// else if (marks >=60){
+//     cout << "D" << endl;
+// }
+// else{
+//     cout << "you failed";
+// }
+
+
+float height;
+cout << "Enter Height in feet" << endl;
+cin >> height;
+
+float weight;
+cout << "Enter your weight" << endl;
+cin >> weight;
+
+if(height > 5){
+
+    if(weight > 50){
+        cout << "You got a good BMI" << endl;
+    }
+    else {
+        cout << "BHai tujhse na ho payega" << endl;
+    }
+
+}
+else {
+    cout << "Complan dila du?" << endl;
 }
 
-if(marks > 90){
-    cout << "A" << endl;
-}
-else if (marks >80){
-cout << "B" << endl; 
-}
-else if (marks >70){
-    cout << "C" << endl;
-}  
-else if (marks >60){
-    cout << "D" << endl;
-}
 
 
 return 0;
