ec8a9082ab4af65f280d1a609c3f0b6012338d69
srivasdev <devanshsri14@hotmail.com>
2026-06-07 17:55:34 +0530

    Break

--- forloop.cpp
+++ forloop.cpp
@@ -3,12 +3,31 @@ using namespace std;
 
 int main (){
 
-// for
-for(int count =1; count <=10; count= count+1) {
-    cout << "Devansh" << endl;
-}
+//  for
+//  for(int count =1; count <=10; count= count+1) {
+//     cout << "Devansh" << endl;
+// }
 
 
 
+// example -
+// for(int i=0; i<=5; i = i + 1){
+//     cout << "Iteration" << i << endl;
+// }
+
+
+// counting 1 to 15
+ for(int i=0; i<=15; i = i+1){
+    cout << i << " ";
+    if(i == 5){
+        break;
+    }
+ }
+
+
+//  counting 51 to 66
+// for(int i=51; i<=66; i=i+1){
+//     cout << i << endl;
+// }
     return 0;
 }
