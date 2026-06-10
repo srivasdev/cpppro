f394b836fb7c232d0d6eb401a22f06953bea1f85
srivasdev <devanshsri14@hotmail.com>
2026-06-10 14:44:07 +0530

    continue

--- forloop.cpp
+++ forloop.cpp
@@ -17,17 +17,25 @@ int main (){
 
 
 // counting 1 to 15
- for(int i=0; i<=12; i = i+2){
-    cout << i << " ";
-    if(i == 5){
-        break;
-    }
- }
+//  for(int i=0; i<=12; i = i+2){
+//     cout << i << " ";
+//     if(i == 5){
+//         break;
+//     }
+//  }
 
 
 //  counting 51 to 66
 // for(int i=51; i<=66; i=i+1){
 //     cout << i << endl;
 // }
+
+for (int i=1; i <=5; i = i+1){
+    if (i == 3){
+        continue;
+    }
+    cout << "Iteration" << endl;
+}
+
     return 0;
 }
